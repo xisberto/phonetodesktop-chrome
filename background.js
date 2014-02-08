f8b803e6f34db5001fe589b1de203f38cde5bf28
ben8p <ben@woow-fr.com>
2014-02-09 00:19:32 +0100

    Add "real time" functionality

--- background.js
+++ background.js
@@ -9,4 +9,31 @@ var oauth = ChromeExOAuth.initBackgroundPage({
 	'callback_page': 'chrome_ex_oauth.html'
 });
 
-oauth.authorize(function(){ });
+var displayed = JSON.parse(localStorage.getItem('list_displayed') || '{}'),
+	get = function() {
+		var list_id = localStorage.getItem('list_id');
+		if (!navigator.onLine || !list_id) return;
+	
+		oauth.sendSignedRequest(
+			"https://www.googleapis.com/tasks/v1/lists/"+list_id+"/tasks",
+			function(resp) {
+				resp = JSON.parse(resp);
+				var j,
+					tasks = resp.items;
+				for(j in tasks) {
+					if(tasks[j].title === "" || displayed[tasks[j].id]) {
+						continue;
+					}
+					chrome.tabs.create({'url': tasks[j].title.split(' ')[0]});
+					displayed[tasks[j].id] = true;
+				}
+				localStorage.setItem('list_displayed', JSON.stringify(displayed));
+			}, {
+				'method': 'GET'
+			}
+		);
+	};
+
+oauth.authorize(function() {
+	setInterval(get, 1e4);
+});
